4a2a4da044381c414e24217bccf43367ae537edb
pestydesperado248 <58351647+pestydesperado248@users.noreply.github.com>
2024-06-03 19:09:02 -0500

    First Music Track

---
+++ Assets/Script/MusicHandler.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MusicHandler : MonoBehaviour
+{
+    public bool StartMusicNow;
+    public bool ResetFilters;
+    public AudioSource musicSource;
+    public AudioClip musicStart;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (StartMusicNow)
+        {
+            StartMusic();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartMusic()
+    {
+        if (musicStart != null)
+        {
+            musicSource.PlayOneShot(musicStart);
+            musicSource.PlayScheduled(AudioSettings.dspTime + musicStart.length);
+        }
+        else
+        {
+            musicSource.Play();
+        }
+    }
+
+    public void QueueNewSong(AudioClip songStart, AudioClip songLoop)
+    {
+        if (musicSource.isPlaying)
+        {
+            musicSource.Stop();
+
+        }
+
+        musicSource.clip = songLoop;
+        if (songStart != null)
+        {
+            musicSource.PlayOneShot(songStart);
+            musicSource.PlayScheduled(AudioSettings.dspTime + songStart.length);
+        }
+        else
+        {
+            musicSource.Play();
+        }
+    }
+
+   
+}
